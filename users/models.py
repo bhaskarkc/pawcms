38184713f5bfbe437c1cb1213e76d5b5d9444eda
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-07 18:40:36 +0545

    added users app

---
+++ users/models.py
@@ -0,0 +1,91 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import Group
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None, full_name=''):
+        # import pdb
+        # pdb.set_trace()
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            username=username,
+            email=UserManager.normalize_email(email),
+            full_name=full_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, full_name=''):
+        """
+        Creates and saves a superuser with the given email, full name and password.
+        """
+        user = self.create_user(
+            username,
+            email,
+            password=password,
+            full_name=full_name,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    full_name = models.CharField(max_length=245)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=254,
+        unique=True,
+        db_index=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, related_name='users', blank=True)
+
+    # USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['full_name', 'email']
+
+    def __unicode__(self):
+        return self.username
+
+    def get_short_name(self):
+        # The user is identified by username
+        return self.username
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_perm(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def email_user(self, subject, message, from_email):
+        pass
+
+    def is_admin(self):
+        return self.is_superuser
+
+    def in_group(self, group_name):
+        try:
+            group = Group.objects.get(name=group_name)
+            return group in self.groups.all()
+        except Group.DoesNotExist:
+            return False
+
+    def __str__(self):
+        return self.full_name
+
+    objects = UserManager()
+
+    class Meta:
+        db_table = u'user'
