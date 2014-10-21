aa8d46ba27b5061bd0758d9b93f86950900b621f
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-21 15:53:38 +0545

    url for dbsettings

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'app.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    (r'', include('page.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       (r'^settings/', include('dbsettings.urls')),
+
+                       (r'', include('page.urls')),
 )
