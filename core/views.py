055cdd0e0cf3d3102bdc99d34834f7df73b50fc9
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-21 18:02:01 +0545

    view for contact form

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response
+from django.core.mail import send_mail, BadHeaderError
+from core.forms import ContactForm
+from socket import error as socket_error
 
 
 def home(request):
@@ -6,7 +10,23 @@ def home(request):
 
 
 def contact(request):
+    message = None
     if request.POST:
-        pass
-    else:
-        return render(request, 'contact.html')
+        subject = request.POST.get('subject', '')
+        message = request.POST.get('message', '')
+        from_email = request.POST.get('email', '')
+
+        if subject and message and from_email:
+            try:
+                send_mail(subject, message, from_email, ['contact@devchuli.edu.np'])
+                message = 'Thank You for the message!'
+            except BadHeaderError:
+                message = 'Invalid Data Submitted!'
+            except socket_error as the_error:
+                if the_error.errno != 111:
+                    raise the_error
+                message = 'Couldn\'t send Email'
+        else:
+            message = 'Please fill in the required fields!'
+            return render(request, 'contact.html', {'message': message})
+    return render(request, 'contact.html', {'message': message})
