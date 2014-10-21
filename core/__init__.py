634d3311cfb6731464a7aa2707ed8f554bfdcaa1
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-21 17:33:30 +0545

    address info from settings

--- core/__init__.py
+++ core/__init__.py
@@ -6,7 +6,7 @@ class CoreSettings(dbsettings.Group):
     site_slogan = dbsettings.StringValue()
     address = dbsettings.TextValue()
     phone = dbsettings.StringValue()
-    fax = dbsettings.StringValue()
+    fax = dbsettings.StringValue(required=False)
     email = dbsettings.StringValue()
 
 
