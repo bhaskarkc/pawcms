fdfcf37d8af2dd5607d60daccb3faf013d6c5eba
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-22 13:56:35 +0545

    url, view, template for news view

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,5 @@ import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.list_news, name='list_news'),
+                       url(r'^(?P<slug>[a-zA-Z0-9_.-]+)/$', views.view_news, name='view_news'),
 )
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from models import News
 
 
@@ -6,3 +6,7 @@ def list_news(request):
     objects = News.objects.filter(status='Published').order_by('-date')
     return render(request, 'list_news.html', {'objects': objects})
 
+
+def view_news(request, slug):
+    obj = get_object_or_404(News, slug=slug, status='Published')
+    return render(request, 'view_news.html', {'obj': obj})
