16e3616bb000d77bd86128d25600e289a71e6e63
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-22 17:50:50 +0545

    Image name and description; and gallery description can be null

--- gallery/models.py
+++ gallery/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Album(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     thumbnail = models.ForeignKey('Image', related_name='thumbnail_of')
 
 
 class Image(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     album = models.ForeignKey(Album)
     file = models.ImageField()
