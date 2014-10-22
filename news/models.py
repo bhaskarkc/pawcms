6b970bc9e599a3dd3afb7c34c2fb18688c84eb0d
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-22 11:41:45 +0545

    News model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from froala_editor.fields import FroalaField
+from users.models import User
+from app.libr import unique_slugify
+import datetime
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(
+        max_length=255,
+        blank=True,
+        null=True,
+        help_text='Leave empty/unchanged for default slug.')
+    content = FroalaField(null=True, blank=True)
+    author = models.ForeignKey(User)
+    statuses = (
+        ('Published', 'Published'), ('Draft', 'Draft'), ('Trashed', 'Trashed'))
+    status = models.CharField(
+        max_length=10,
+        choices=statuses,
+        default='Published')
+    comments_enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField(editable=False)
+    updated_at = models.DateTimeField(editable=False)
+
+    def save(self, *args, **kwargs):
+        unique_slugify(self, self.title)
+        if not self.id:
+            self.created_at = datetime.datetime.today()
+        self.updated_at = datetime.datetime.today()
+        super(News, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
