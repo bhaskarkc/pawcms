26fd457c6ab110977bf8ea3721188ce2c51f12b0
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-28 19:07:14 +0545

    Custom Template Loader

--- core/loader.py
+++ core/loader.py
@@ -5,69 +5,39 @@ from django.conf import settings
 from django.utils._os import safe_join
 
 
-class Loader(BaseLoader):
-    # is_usable = True
-    #
-    # def get_template_sources(self, template_name):
-    # template_dirs = settings.TEMPLATE_DIRS
-    # for template_dir in template_dirs:
-    # new_dir = os.path.join(template_dir, self.theme)
-    # try:
-    # yield safe_join(new_dir, template_name)
-    # except UnicodeDecodeError:
-    #             raise
-    #         except ValueError:
-    #             pass
-    #
-    # def load_template_source(self, template_name, template_dirs=None):
-    #     tried = []
-    #     print 'hey'
-    #     self.theme = core_settings.theme
-    #     for filepath in self.get_template_sources(template_name):
-    #         try:
-    #             with open(filepath, 'rb') as fp:
-    #                 return (fp.read().decode(settings.FILE_CHARSET), filepath)
-    #         except IOError:
-    #             tried.append(filepath)
-    #     if tried:
-    #         error_msg = "Tried %s" % tried
-    #     else:
-    #         error_msg = "Your TEMPLATE_DIRS setting is empty. Change it to point to at least one template directory."
-    #     raise TemplateDoesNotExist(error_msg)
-    #
-    # load_template_source.is_usable = True
+def calculate_custom_template_dirs():
+    theme = core_settings.theme
+    if not theme:
+        return ()
+    template_dirs = settings.TEMPLATE_DIRS
+    possible_dirs = []
+    for template_dir in template_dirs:
+        possible_dirs.append(os.path.join(template_dir, theme))
+    return tuple(possible_dirs)
 
+# At compile time, cache the directories to search.
+custom_template_dirs = calculate_custom_template_dirs()
+
+
+class ThemeLoader(BaseLoader):
     is_usable = True
 
-    def get_template_source(self, template_name):
-        # template_dir = settings.TEMPLATE_DIRS[0]
-        return '/home/xtranophilist/pro/paw/app/templates/base2013/base.html'
-        # for template_dir in template_dirs:
-        #     new_path = os.path.join(template_dir, self.theme, template_name)
-        #     print new_path
-        #     try:
-        #         yield new_path
-        #     except UnicodeDecodeError:
-        #         raise
-        #     except ValueError:
-        #         pass
+    def get_template_sources(self, template_name, template_dirs=None):
+        if not template_dirs:
+            template_dirs = custom_template_dirs
+        for template_dir in template_dirs:
+            try:
+                yield safe_join(template_dir, template_name)
+            except UnicodeDecodeError:
+                raise
+            except ValueError:
+                pass
 
     def load_template_source(self, template_name, template_dirs=None):
-        tried = []
-        self.theme = core_settings.theme
-        print 'bye'
-        # for filepath in self.get_template_sources(template_name, template_dirs):
-        filepath = self.get_template_source(template_name)
-        try:
-            with open(filepath, 'rb') as fp:
-                return (fp.read().decode(settings.FILE_CHARSET), filepath)
-        except IOError:
-            tried.append(filepath)
-        if tried:
-            error_msg = "Tried %s" % tried
-        else:
-            error_msg = "Your TEMPLATE_DIRS setting is empty. Change it to point to at least one template directory."
-        raise TemplateDoesNotExist(error_msg)
-
-    load_template_source.is_usable = True
-
+        for filepath in self.get_template_sources(template_name, template_dirs):
+            try:
+                with open(filepath, 'rb') as fp:
+                    return (fp.read().decode(settings.FILE_CHARSET), filepath)
+            except IOError:
+                pass
+        raise TemplateDoesNotExist(template_name)
