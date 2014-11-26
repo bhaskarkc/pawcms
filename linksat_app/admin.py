522289466942798e6b735811430415cbc1117c07
Dipesh Acharya <xtranophilist@gmail.com>
2014-11-26 13:18:38 +0545

    linksat app in admin

--- linksat_app/admin.py
+++ linksat_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, Customer, Brand, Solution
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Brand)
+admin.site.register(Solution)
